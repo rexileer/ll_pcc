96f78ee97759c59902adea53b81bef30777d0a18
rexileer <rexileer@experience.com>
2022-10-18 19:09:42 +0300

    Added get_object_or_404

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -16,7 +17,7 @@ def topics(request):
 
 @login_required    
 def topic(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     check_topic_owner(topic, request)
     entries = topic.entry_set.order_by('-date_added')   
     context = {'topic': topic, 'entries': entries}
