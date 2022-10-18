214718da664b6e33047d7894278e040f8857e2bf
rexileer <rexileer@experience.com>
2022-10-18 17:33:16 +0300

    Ready for deployment to heroku.

---
+++ learning_logs/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Homepage
+    path('', views.index, name='index'),
+    # topics page
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной информацией по отдельной теме
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Страница для добавления новой темы
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Страница для добавления новой записи
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Страница для редактирования записи
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+]
